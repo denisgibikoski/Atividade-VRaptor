c12e503aca1174a46c5126914d8f44af4c4945fe
Denis <d3nys.inf@gmail.com>
2018-08-12 22:24:10 -0300

    Atividade Vraptor Rest Problemas em save()

--- target/generated-sources/annotations/com/d3nys/atividadevraptor/model/Livro_.java
+++ target/generated-sources/annotations/com/d3nys/atividadevraptor/model/Livro_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-12T22:05:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-12T22:15:29")
 @StaticMetamodel(Livro.class)
 public class Livro_ { 
 
